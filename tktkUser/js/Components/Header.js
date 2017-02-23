a40989d94c454747c4da47fb2957c80b05953472
Poste7 <Poste7@iMac-de-Poste7.local>
2017-02-23 10:31:02 +0100

    Code plus propre

--- tktkUser/js/Components/Header.js
+++ tktkUser/js/Components/Header.js
@@ -1,3 +1,7 @@
+// ----------------------------------------
+// ----- Imports
+
+// npm
 import React, {Component} from 'react';
 import {View, Image} from 'react-native';
 import {Header} from 'native-base';
@@ -5,25 +9,29 @@ import LinearGradient from 'react-native-linear-gradient';
 
 export default class CustomHeader extends Component {
 
-   render() {
-      const start = {
-         x: 0.0,
-         y: 0.0
-      };
+  
+  // ----------------------------------------
+  // ----- Render
 
-      const end = {
-         x: 1.0,
-         y: 1.0
-      };
+  render() {
+    const start = {
+      x: 0.0,
+      y: 0.0
+    };
 
-      const colors = ['#E53919', '#EC5E18'];
+    const end = {
+      x: 1.0,
+      y: 1.0
+    };
 
-      return (
-         <LinearGradient start={start} end={end} colors={colors}>
-            <Header>
-               {this.props.children}
-            </Header>
-         </LinearGradient>
-      );
-   }
+    const colors = ['#E53919', '#EC5E18'];
+
+    return (
+      <LinearGradient start={start} end={end} colors={colors}>
+        <Header>
+          {this.props.children}
+        </Header>
+      </LinearGradient>
+    );
+  }
 }
--- tktkUser/js/Components/Logo.js
+++ tktkUser/js/Components/Logo.js
@@ -1,35 +1,47 @@
+// ----------------------------------------
+// ----- Imports
+
+// npm
 import React, {Component} from 'react';
 import {View, Image} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
 export default class Logo extends Component {
 
-   render() {
-      return (
-         <View style={nativeStyles.logoContainer}>
-            <LinearGradient start={{
-               x: 0.0,
-               y: 0.0
-            }} end={{
-               x: 0.5,
-               y: 1.0
-            }} colors={['#E53919', '#EC5E18']} style={nativeStyles.logoSubContainer}>
-               <Image source={require('../../images/logo.png')}/>
-            </LinearGradient>
-         </View>
-      );
-   }
+
+  // ----------------------------------------
+  // ----- Render
+
+  render() {
+    return (
+      <View style={nativeStyles.logoContainer}>
+        <LinearGradient start={{
+            x: 0.0,
+            y: 0.0
+          }} end={{
+            x: 0.5,
+            y: 1.0
+          }} colors={['#E53919', '#EC5E18']} style={nativeStyles.logoSubContainer}>
+          <Image source={require('../../images/logo.png')}/>
+        </LinearGradient>
+      </View>
+    );
+  }
 }
 
+
+// ----------------------------------------
+// ----- Styles
+
 var nativeStyles = {
-   logoContainer: {
-      marginTop: 35,
-      marginBottom: 25,
-      justifyContent: 'center',
-      alignItems: 'center'
-   },
-   logoSubContainer: {
-      padding: 20,
-      borderRadius: 40
-   }
+  logoContainer: {
+    marginTop: 35,
+    marginBottom: 25,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  logoSubContainer: {
+    padding: 20,
+    borderRadius: 40
+  }
 }
